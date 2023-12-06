e4175a0ed5030769045abd23d055523fd605486b
Ana <anmeuri@gmail.com>
2023-12-05 23:32:19 -0800

    Add a limit to the back function w/ test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,18 +4,17 @@ import { useState } from 'react';
 const useVisualMode = (initial) => {
   const [history, setHistory] = useState([initial]);
 
-
   const transition = (newState) => {
     setHistory(prev => [...prev, newState]);
   };
 
-  const back = () => { 
-    setHistory(prev => [...prev.slice(0, prev.length - 1)]); 
-  }
-
+  const back = () => {
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    }
+  };
 
   return { mode: history[history.length - 1], transition, back };
-
 };
 
 export default useVisualMode;
